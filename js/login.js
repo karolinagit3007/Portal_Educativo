f4f61724b430ac9fbac43d34f2089288d1d3faca
karolinagit3007 <karolina.17hidalgo@gmail.com>
2025-01-15 20:49:56 -0500

    Subir proyecto inicial al repositorio

---
+++ js/login.js
@@ -0,0 +1,25 @@
+// Usuarios y contraseñas predefinidos
+const users = [
+    { email: "monica@correo.com", password: "123456" },
+    { email: "roberto@correo.com", password: "abcdef" }
+];
+
+// Manejo del formulario de inicio de sesión
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita el envío del formulario
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("errorMessage");
+
+    // Validación de credenciales
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        // Redirigir al usuario a la página "informacion.html"
+        window.location.href = "pages/informacion.html";
+    } else {
+        // Mostrar mensaje de error
+        errorMessage.style.display = "block";
+    }
+});
