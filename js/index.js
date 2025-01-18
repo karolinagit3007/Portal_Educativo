80437a04d43bf3497827b9e3955180d713b09157
karolinagit3007 <karolina.17hidalgo@gmail.com>
2025-01-17 22:30:22 -0500

    lista desplegable de identificación

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// Contador animado para estadísticas
+const counters = document.querySelectorAll('.stat-number');
+const speed = 200; // Velocidad del contador
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / speed;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
